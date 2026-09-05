1e2f3e16c3653df806f32f411719d02b9f5f5f54
羅子淇 Jacky Law, F.I.H.R.M.(HK), FHKIoD <289607996+jackylawck@users.noreply.github.com>
2026-09-05 11:18:26 +0800

    Update vite.config.ts

--- web-frontend/vite.config.ts
+++ web-frontend/vite.config.ts
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import wasm from 'vite-plugin-wasm';
-import topLevelAwait from 'vite-plugin-top-level-await';
 
 export default defineConfig({
-  plugins: [react(), wasm(), topLevelAwait()],
+  plugins: [react(), wasm()],
   base: './', // 確保部署到 GitHub Pages 時相對路徑正確
+  build: {
+    target: 'esnext', // 讓 Rollup/Vite 原生輸出支援 WebAssembly 和 top-level await 的 modern JS
+  },
+  esbuild: {
+    supported: {
+      'top-level-await': true, // 避免 esbuild 轉譯時報錯
+    },
+  },
 });
