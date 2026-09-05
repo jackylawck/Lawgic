1c2d31b8b06eb80b44b8c8c4d096d3436535f70b
羅子淇 Jacky Law, F.I.H.R.M.(HK), FHKIoD <289607996+jackylawck@users.noreply.github.com>
2026-09-05 11:22:47 +0800

    Update vite.config.ts

--- web-frontend/vite.config.ts
+++ web-frontend/vite.config.ts
@@ -4,13 +4,13 @@ import wasm from 'vite-plugin-wasm';
 
 export default defineConfig({
   plugins: [react(), wasm()],
-  base: './', // 確保部署到 GitHub Pages 時相對路徑正確
+  base: './',
   build: {
-    target: 'esnext', // 讓 Rollup/Vite 原生輸出支援 WebAssembly 和 top-level await 的 modern JS
+    target: 'esnext',
   },
   esbuild: {
     supported: {
-      'top-level-await': true, // 避免 esbuild 轉譯時報錯
+      'top-level-await': true,
     },
   },
 });
