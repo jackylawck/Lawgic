05dd79c2a2f0865d9c7b4ce6ccfcd88c8eebcc10
羅子淇 Jacky Law, F.I.H.R.M.(HK), FHKIoD <289607996+jackylawck@users.noreply.github.com>
2026-09-11 16:50:28 +0800

    Update postcss.config.js

--- web-frontend/postcss.config.js
+++ web-frontend/postcss.config.js
@@ -1,13 +1,8 @@
-export default () => {
-  return {
-    plugins: {
-      'tailwindcss/nesting': {},
-      tailwindcss: {},
-      autoprefixer: {
-        // 確保 flexbox 與 grid 在舊版瀏覽器或 iOS 舊版 Safari 上的前綴補齊
-        flexbox: 'no-2009',
-        grid: 'autoplace',
-      },
+export default {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {
+      flexbox: 'no-2009',
     },
-  };
+  },
 };
